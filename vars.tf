d18b3ba000c68387c657f9a853e866394af27a6b
jagandhandapani <66021139+jagandhandapani@users.noreply.github.com>
2020-06-12 15:32:39 +0530

    Update vars.tf
    ZD

--- vars.tf
+++ vars.tf
@@ -19,8 +19,10 @@ variable "web_server_subnet_name" {
 
 variable "web_server_subnet_address_space" {
   type = "string"
+  default = "10.0.1.0/24"
 }
 
 variable "app_server_subnet_address_space" {
   type = "string"
+  default = "10.0.2.0/24"
 }
