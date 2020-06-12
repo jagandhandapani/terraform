0cfa89d7644e7312ce13ee2c4c02a2b213deb4b9
jagandhandapani <66021139+jagandhandapani@users.noreply.github.com>
2020-06-12 15:35:42 +0530

    Update vars.tf
    zdv

--- vars.tf
+++ vars.tf
@@ -1,20 +1,25 @@
 variable "rgroup" {
   type = "string"
+  default = "Test_RG"
 }
 
 variable "location" {
   type = "string"
+  default = "EastUS"
 }
 
 variable "prefix" {
   type = "string"
+  default = "test"
 }
 variable "vnet_address_space" {
   type = "string"
+  default = "10.0.0.0/16" 
 }
 
 variable "web_server_subnet_name" {
   type = "string"
+  default = "web_server_subnet"
 }
 
 variable "web_server_subnet_address_space" {
